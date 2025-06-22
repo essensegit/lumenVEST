0504f4e75ccc889cddb1cb2ebd68b74e72e461af
Essense <hy.essense@gmail.com>
2025-06-22 20:15:46 +0100

    removed ajax error

--- application/views/Pages/signup.php
+++ application/views/Pages/signup.php
@@ -158,7 +158,7 @@
     <script src="<?php echo base_url('assets/js/vendor/swiper-bundle.min.js')?>"></script>
     <script src="<?php echo base_url('assets/js/script.js')?>"></script>
     <script src="<?php echo base_url('assets/js/swiper-script.js')?>"></script>
-    <script src="<?php echo base_url('assets/js/submit-form.js')?>"></script>
+    <!-- <script src="<?php echo base_url('assets/js/submit-form.js')?>"></script> -->
     <script src="<?php echo base_url('assets/js/vendor/isotope.pkgd.min.js')?>"></script>
     <script src="<?php echo base_url('assets/js/video_embedded.js')?>"></script>
     <script src="<?php echo base_url('assets/js/vendor/fslightbox.js')?>"></script>
