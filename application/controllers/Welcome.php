11b988b46f4e52686d94dbaf439cb6cedc619558
Essense <hy.essense@gmail.com>
2025-06-22 20:36:12 +0100

    debugging db save problems

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,21 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/userguide3/general/urls.html
-	 */
 	public function index()
 	{
 		$this->load->view('Pages/header');
@@ -40,27 +25,38 @@ class Welcome extends CI_Controller {
         $this->form_validation->set_rules('password', 'Password', 'required|min_length[6]');
         $this->form_validation->set_rules('confirm_password', 'Confirm Password', 'required|matches[password]');
 
-        if ($this->form_validation->run()) {
+		if ($this->form_validation->run()) {
 			echo "Validation passed<br>";
-            $userData = [
-                'email' => $this->input->post('email'),
-                'firstname' => $this->input->post('firstname'),
-                'lastname' => $this->input->post('lastname'),
-                'phonenumber' => $this->input->post('phonenumber'),
-                'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT)
-            ];
 
-            if ($this->User_model->insert_user_with_wallet($userData)) {
-				echo "User inserted!<br>";
-                $this->session->set_flashdata('success', 'Signup successful! You can now login.');
-                redirect('signup');
-            } else {
-				 echo "Insert failed<br>";
-                $this->session->set_flashdata('error', 'Something went wrong. Try again.');
-            } 
+			echo "<pre>";
+			print_r($this->input->post());
+			echo "</pre>";
+
 		} else {
-            echo "Validation failed<br>";
-        }
+			echo "Validation failed:<br>";
+			echo validation_errors(); // Show exactly what failed
+		}
+        // if ($this->form_validation->run()) {
+		// 	echo "Validation passed<br>";
+        //     $userData = [
+        //         'email' => $this->input->post('email'),
+        //         'firstname' => $this->input->post('firstname'),
+        //         'lastname' => $this->input->post('lastname'),
+        //         'phonenumber' => $this->input->post('phonenumber'),
+        //         'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT)
+        //     ];
+
+        //     if ($this->User_model->insert_user_with_wallet($userData)) {
+		// 		echo "User inserted!<br>";
+        //         $this->session->set_flashdata('success', 'Signup successful! You can now login.');
+        //         redirect('signup');
+        //     } else {
+		// 		 echo "Insert failed<br>";
+        //         $this->session->set_flashdata('error', 'Something went wrong. Try again.');
+        //     } 
+		// } else {
+        //     echo "Validation failed<br>";
+        // }
         
     }
 		$this->load->view('Pages/header');
